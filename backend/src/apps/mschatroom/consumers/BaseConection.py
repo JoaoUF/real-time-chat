42ee4d606fcf41faaf07ee765b455aa32038e9b6
Joao Urrunaga <u201913507@upc.edu.pe>
2024-09-17 14:35:34 -0500

    base conection fixed

--- backend/src/apps/mschatroom/consumers/BaseConection.py
+++ backend/src/apps/mschatroom/consumers/BaseConection.py
@@ -1,26 +1,38 @@
-from channels.generic.websocket import AsyncConsumer
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
 from django.core.exceptions import ObjectDoesNotExist
 from channels.db import database_sync_to_async
 from mschatroom.models import ConnectionHistory
+from msauthentication.models import CustomUser
 
 
-class BaseConection(AsyncConsumer):
+class BaseConection(AsyncWebsocketConsumer):
 
-    async def websocket_connect(self, event):
+    async def connect(self):
         print("base-connection-stablish")
-        user = self.scope["user"]
-        self.update_user_status(user, "online")  # type: ignore
+        self.user = self.scope["url_route"]["kwargs"]["user"]
+        await self.update_user_status(self.user, "online")
+        await self.accept()
 
-    async def websocket_disconnect(self, event):
+    async def disconnect(self, event):
         print("base-connection-finished")
-        user = self.scope["user"]
-        self.update_user_status(user, "offline")  # type: ignore
+        self.user = self.scope["url_route"]["kwargs"]["user"]
+        await self.update_user_status(self.user, "offline")
+        await self.close()
+
+    async def chat_message(self, event) -> None:
+        await self.send(text_data=json.dumps(event["message"]))
 
     @database_sync_to_async
-    def update_user_status(self, user, status):
+    def update_user_status(self, user, set_status) -> None:
         try:
-            connection = ConnectionHistory.objects.get(user=user)
-            connection.status = status
-            return connection.save()
+            current_user = CustomUser.objects.get(id=user)
         except ObjectDoesNotExist:
-            return ConnectionHistory(user=user).save()
+            raise
+
+        current_history, create = ConnectionHistory.objects.get_or_create(
+            user=current_user
+        )
+        if not create:
+            current_history.status = set_status
+            current_history.save()
--- backend/src/apps/mschatroom/routing.py
+++ backend/src/apps/mschatroom/routing.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from mschatroom.consumers import BaseConection
 
 websocket_urlpatterns = [
-    path("chat/<int:user>/", BaseConection.as_asgi()),
+    # path("chat/<int:user>/", BaseConection.as_asgi()),
+    re_path(r"ws/chat/(?P<user>\w+)/$", BaseConection.as_asgi()),
 ]
