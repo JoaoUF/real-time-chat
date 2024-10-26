8ebeab584e555732272dbe2e81fdf19b46e2e51d
Joao Urrunaga <u201913507@upc.edu.pe>
2024-10-25 19:12:08 -0500

    change path, (rename, order & optimize) @actions

--- backend/src/apps/mschatroom/consumers/CustomUserConsumer.py
+++ backend/src/apps/mschatroom/consumers/CustomUserConsumer.py
@@ -1,55 +1,41 @@
-from django.http import JsonResponse
 from djangochannelsrestframework.generics import GenericAsyncAPIConsumer
-from djangochannelsrestframework.mixins import (
-    ListModelMixin,
-    CreateModelMixin,
-    DeleteModelMixin,
-)
 from django.core.exceptions import ObjectDoesNotExist
 from channels.db import database_sync_to_async
-from mschatroom.models import UserChat, ConnectionHistory, Message
+from mschatroom.models import UserChat, ConnectionHistory
 from msauthentication.models import CustomUser
 from msauthentication.serializers import CustomUserSerializerBaseProfile
-from mschatroom.serializers import (
-    MessageSerializerLastMessage,
-    ConnectionHistorySerializer,
-    UserChatCustomUserMessageSerializer,
-)
+from mschatroom.serializers import UserChatCustomUserMessageSerializer
 from djangochannelsrestframework.observer import model_observer
 from djangochannelsrestframework.decorators import action
-
-
-"""
-DOTO:
-- create a disconnect function which erase user_id and change to offline
-"""
 
 
 class CustomUserConsumer(GenericAsyncAPIConsumer):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializerBaseProfile
 
-    async def disconnect(self, code):
-        if hasattr(self, "user_id"):
-            await self.change_user_connection_status(new_status="offline")
-            await self.notify_change_user_connection_history()
-        return await super().disconnect(code)
-
-    @action()
-    async def subscribe_to_connection_history(self, id, request_id, **kwargs):
+    async def connect(self):
+        id = self.scope["url_route"]["kwargs"]["id"]
         if await self.check_custom_user_exist(pk=id):
             self.user_id = id
+            return await super().connect()
         else:
-            raise
-        await self.connection_history_activity.subscribe(request_id=request_id)  # type: ignore
+            return {}, 404
 
     @action()
-    async def change_connection_history_online(self, **kwargs):
-        await self.change_user_connection_status(new_status="online")
+    async def subscribe_to_observer(self, request_id, session=None, **kwargs):
+        await self.connection_history_activity.subscribe(session=session, request_id=request_id)  # type: ignore
+
+    @action()
+    async def unsubscribe_to_observer(self, request_id, **kwargs):
+        await self.connection_history_activity.unsubscribe(request_id=request_id)  # type: ignore
+
+    @action()
+    async def change_user_status(self, request_id, status, **kwargs):
+        await self.change_user_connection_status(new_status=status)
         await self.notify_change_user_connection_history()
 
     @action()
-    async def list_user_chat(self, **kwarg):
+    async def list_user_chat(self, request_id, **kwarg):
         listUserChat = await self.get_filter_list_user_chat(pk=self.user_id)
         await self.send_json(
             {
@@ -60,19 +46,16 @@ class CustomUserConsumer(GenericAsyncAPIConsumer):
 
     @model_observer(ConnectionHistory)
     async def connection_history_activity(  # type: ignore
-        self,
-        message,
-        observer: None,
-        subscribing_request_ids=[],
-        **kwargs,
+        self, message, observer: None, subscribing_request_ids=[], **kwargs
     ):
         pass
 
     @connection_history_activity.groups_for_consumer  # type: ignore
-    def connection_history_activity(self, sesion=None, **kwargs):
-        if sesion is not None:
-            print(f"-user__{sesion}")
-            yield f"-user__{sesion}"
+    def connection_history_activity(self, session=None, **kwargs):
+        if session is not None:
+            print(f"-request_id__{session}")
+            yield f"-request_id__{session}"
+            yield f"-other_id__{session}"
 
     async def notify_change_user_connection_history(self):
         print("notify other users")
@@ -117,7 +100,7 @@ class CustomUserConsumer(GenericAsyncAPIConsumer):
         return CustomUser.objects.filter(pk=pk).exists()
 
     @database_sync_to_async
-    def change_user_connection_status(self, new_status) -> None:
+    def change_user_connection_status(self, new_status):
         try:
             current_user = CustomUser.objects.get(id=self.user_id)
             current_history, create = ConnectionHistory.objects.get_or_create(
@@ -127,4 +110,4 @@ class CustomUserConsumer(GenericAsyncAPIConsumer):
                 current_history.status = new_status
                 current_history.save()
         except ObjectDoesNotExist:
-            raise
+            return {}, 400
--- backend/src/apps/mschatroom/routing.py
+++ backend/src/apps/mschatroom/routing.py
@@ -2,7 +2,5 @@ from django.urls import path, re_path
 from mschatroom.consumers import CustomUserConsumer
 
 websocket_urlpatterns = [
-    # path("chat/<int:user>/", BaseConection.as_asgi()),
-    # re_path(r"ws/chat/(?P<user>\w+)/$", BaseConection.as_asgi()),
-    re_path(r"ws/chat/$", CustomUserConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<id>\w+)/$", CustomUserConsumer.as_asgi()),
 ]
